8e876815fca14a954bce27773619d114a9d8810f
kygm <rickev20@gmail.com>
2024-11-03 17:50:38 -0600

    added initialization feaeture to wait for a response from the heater before sending any commands, adjusted temperature readings, updated user interface

--- RxTempReadingsToggleHeater/RxTempReadingsToggleHeater.ino
+++ RxTempReadingsToggleHeater/RxTempReadingsToggleHeater.ino
@@ -17,6 +17,7 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 const uint8_t BUTTON_PIN = 13;
 bool startHeaterCommanded = false;
+bool isFirstRun = true;
 
 
 uint8_t macAddrToSendMsgTo[] = {0xD4, 0x8A, 0xFC, 0xA5, 0xEA, 0xEC};
@@ -50,8 +51,19 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&incomingHeaterPayload, incomingData, sizeof(incomingHeaterPayload));
   Serial.print("Bytes received: ");
   Serial.println(len);
+  //kygm workaround for temp difference
+  incomingHeaterPayload.Temperature -= 8;
 
   Serial.println(incomingHeaterPayload.Temperature);
+
+  //Need to do this to handle when the heater is on and this device loses power/is shut off.
+  //otherwise since startHeaterCommand is initialized to 0, the heater will be immediately shut off
+  //on first tx
+  if(isFirstRun)
+  {
+    startHeaterCommanded = incomingHeaterPayload.StartHeater;
+    isFirstRun = false;
+  }
 
   Serial.print("Rx success! \n");
 }
@@ -100,23 +112,58 @@ void setup() {
   esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
 }
  
-void loop() {
- 
-  outgoingHeaterPayload.StartHeater = startHeaterCommanded;
+int numberOfWaitCycles = 0;
+const int NUMBER_CYCLE_TO_WAIT_FOR = 5;
 
-  // Send message via ESP-NOW
-  esp_err_t result = esp_now_send(macAddrToSendMsgTo, (uint8_t *) &outgoingHeaterPayload, sizeof(outgoingHeaterPayload));
-   
-  if (result == ESP_OK) {
-    Serial.println("Sent with success");
+void loop() {
+  
+  //Waiting five wait cycles to get a response from the heater device in case this device
+  //was restarted while the heater device was commanded on 
+  if(isFirstRun && numberOfWaitCycles <= NUMBER_CYCLE_TO_WAIT_FOR)
+  {
+    numberOfWaitCycles++;
+    displayInitialization();
+
+    //resetting to wait another five cycles for a response. Otherwise the device 
+    //shows zeros as data
+    if(numberOfWaitCycles == NUMBER_CYCLE_TO_WAIT_FOR)
+      numberOfWaitCycles = 0;
   }
-  else {
-    Serial.println("Error sending the data");
+  else
+  {
+    outgoingHeaterPayload.StartHeater = startHeaterCommanded;
+
+    // Send message via ESP-NOW
+    esp_err_t result = esp_now_send(macAddrToSendMsgTo, (uint8_t *) &outgoingHeaterPayload, sizeof(outgoingHeaterPayload));
+    
+    if (result == ESP_OK) {
+      Serial.println("Sent with success");
+    }
+    else {
+      Serial.println("Error sending the data");
+    }
+
+    updateDisplay();
   }
-  updateDisplay();
   delay(1000);
 }
 
+void displayInitialization()
+{
+  display.clearDisplay();
+  display.setCursor(0, 5);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.println("Initializing...");
+  display.println("Waiting on a response from the heater");
+  display.println();
+
+  display.print("Attempt count: ");
+  display.print(numberOfWaitCycles);
+  display.print("/");
+  display.print(NUMBER_CYCLE_TO_WAIT_FOR);
+  display.display();
+}
 
 void updateDisplay(){
 
@@ -124,26 +171,28 @@ void updateDisplay(){
   display.setTextSize(1);
   display.setTextColor(WHITE);
 
-  display.setCursor(0, 0);
-  display.println("INCOMING READINGS");
+  display.setCursor(0, 5);
+  display.println("  KYGM Services LLC");
 
   display.setCursor(0, 15);
-  display.print("Temperature: ");
-  display.print(incomingHeaterPayload.Temperature);
+  display.print("Incoming Data");
+
+  display.setCursor(0, 25);
+  display.print("- Temperature: ");
+  display.print((int)incomingHeaterPayload.Temperature);
   display.cp437(true);
   display.write(248);
   display.print("F");
 
-  display.setCursor(0, 25);
-  display.print("Is heater on?: ");
+  display.setCursor(0, 35);
+  display.print("- Heater On:   ");
   display.print(incomingHeaterPayload.IsHeaterRunning);
 
-  display.setCursor(0, 35);
-  display.print("Start heater?: ");
-  display.print(incomingHeaterPayload.StartHeater);
+  display.setCursor(0, 45);
+  display.print("Outgoing Data");
 
-  display.setCursor(0, 56);
-  display.print("Cmd Heater? ");
+  display.setCursor(0, 55);
+  display.print("- Command On:  ");
   display.print(startHeaterCommanded);
   display.display();
   
